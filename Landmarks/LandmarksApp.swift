489aed15bb0e2f9e44646e1da74b50595d480136
nazarIvzhenko <nazarivzhenko@icloud.com>
2023-01-25 15:38:33 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nazar Ivzhenko on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
