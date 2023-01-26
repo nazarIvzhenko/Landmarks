be98ce4f6cadc24e21aa7a8588161d6bdd2ab54a
nazarIvzhenko <nazarivzhenko@icloud.com>
2023-01-26 14:41:54 +0200

    Created a badge that the user will receive when visiting the desired landmark

---
+++ Landmarks/Views/Badges/Badge.swift
@@ -0,0 +1,38 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Nazar Ivzhenko on 26.01.2023.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbol: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbol
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
